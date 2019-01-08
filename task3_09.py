0587237035d051e0dfeb07a60b58b3152d7b6ed4
Kirill <arist84@mail.ru>
2019-01-08 16:54:43 +0300

    PA HW3 task03_09 complete

--- task3_09.py
+++ task3_09.py
@@ -4,4 +4,27 @@ __author__ = 'AKV'
 """
 9. Найти максимальный элемент среди минимальных элементов столбцов матрицы.
 """
+import random
+array = [[random.randint(0, 100) for i in range(6)] for j in range(5)]
 
+print(array)
+
+j = 0
+while j < len(array[0]):
+    min_in_column = array[0][j]
+
+    i = 1
+    while i < len(array):
+        if array[i][j] < min_in_column:
+            min_in_column = array[i][j]
+        i += 1
+
+    print(min_in_column)
+    if j == 0:
+        max_in_min = min_in_column
+    else:
+        if max_in_min < min_in_column:
+            max_in_min = min_in_column
+    j += 1
+
+print(f"Максимальный элемент среди минимальных элементов столбцов таблицы равен {max_in_min}")
